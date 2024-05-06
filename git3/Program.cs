5dbd217a8158e1e944e489fa68943e4c7d0cdfad
amisha <amishasurve23@gmail.com>
2024-05-06 10:30:50 +0530

    Add project files.

---
+++ git3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pp = new Program();
+            pp.m1();
+
+        }
+        public void m1()
+        {
+            int x = 50;
+            int y= 50;
+            int z = x + y;
+            Console.WriteLine(z);
+        }
+    }
+}
